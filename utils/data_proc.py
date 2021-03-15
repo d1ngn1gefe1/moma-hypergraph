43eb95b924f923cf38f1f66c86dd6de0c180057e
d1ngn1gefe1 <alanzluo@gmail.com>
2021-03-15 03:21:46 -0700

    remove old function

--- utils/data_proc.py
+++ utils/data_proc.py
@@ -5,7 +5,6 @@ from torch._six import container_abcs, string_classes, int_classes
 from torch_geometric.data import Batch, Data
 
 
-
 def cat_vl(tensor_list):
   """ Concatenate tensors of varying lengths
   :param tensor_list: a list of tensors of varying tensor.shape[0] but same tensor.shape[1:]
@@ -78,44 +77,3 @@ def to_pyg_data(trim_ann, feat, y):
   delattr(data, 'batch')
 
   return data
-
-
-# def to_pyg_data(ag, subactivity_cid, num_actor_classes, num_object_classes, num_relat_classes):
-#   actor_cids =
-#   entity_cids = [actor['cid'] for actor in ag['actors']]+ \
-#               [object['cid']+num_actor_classes for object in ag['objects']]
-#   instance_ids = [actor['iid'] for actor in ag['actors']]+ \
-#                  [object['iid'] for object in ag['objects']]
-#
-#   num_node_features = num_actor_classes+num_object_classes
-#   num_edge_features = num_relat_classes
-#
-#   x = np.zeros((len(entity_cids), num_node_features), dtype=np.float32)
-#   x[np.arange(len(entity_cids)), entity_cids] = 1
-#
-#   edges, edge_cids = [], []
-#   for relat in graph['relationships']:
-#     source_node_instance_ids, sink_node_instance_ids = relat['description'].split('),(')
-#     source_node_instance_ids = np.array(source_node_instance_ids[1:].split(','))
-#     sink_node_instance_ids = np.array(sink_node_instance_ids[:-1].split(','))
-#     instance_ids = np.array(instance_ids)
-#     source_node_ids = np.nonzero(source_node_instance_ids[:, None] == instance_ids)[1]
-#     sink_node_ids = np.nonzero(sink_node_instance_ids[:, None] == instance_ids)[1]
-#
-#     edge_cid = relat['relat_cid']
-#
-#     edge = np.array(np.meshgrid(source_node_ids, sink_node_ids)).T.reshape(-1, 2)
-#     edges.append(edge)
-#     edge_cids += [edge_cid]*edge.shape[0]
-#   edges = np.concatenate(edges, 0)
-#
-#   edge_index = edges.T.astype(np.int64)
-#   edge_type = np.array(edge_cids, dtype=np.float32)
-#
-#   y = np.array([subactivity_cid], dtype=np.int64)
-#
-#   data = Data(x=torch.from_numpy(x),
-#               edge_index=torch.from_numpy(edge_index),
-#               edge_attr=torch.from_numpy(edge_type),
-#               y=torch.from_numpy(y))
-#   return data
